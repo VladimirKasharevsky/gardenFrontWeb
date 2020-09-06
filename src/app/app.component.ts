4e97f58b0adc63bd8aec4d83e472fcb7fe9247b0
VladimirKasharevsky <kashar015@rambler.ru>
2020-09-06 20:20:10 +0300

    Created app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gardenfront';
+  userId: string = '';
+  response: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  // tslint:disable-next-line:typedef
+  search(){
+    this.http.get('http://localhost:8080/user/' + this.userId, )
+      .subscribe((response) => {
+        this.response = response;
+        console.log(this.response);
+      });
+  }
 }
